8be5d9c793717c8c006c3b8a6363f2ab2b8b50b7
Ekaterina Epiphanova <epiph@yandex.ru>
2022-07-30 10:04:45 +0500

    fix: use switch in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
 function Header({email, onLogout}) {
   const [burgerIsOpened, setBurgerIsOpened] = useState(false)
@@ -11,41 +11,43 @@ function Header({email, onLogout}) {
   return (
     <header className="header">
       <Link to="/" className="header__logo"></Link>
-      <Route path="/sign-in">
-        <Link to="/sign-up" className="header__link">
-          Регистрация
-        </Link>
-      </Route>
-      <Route path="/sign-up">
-        <Link to="/sign-in" className="header__link">
-          Войти
-        </Link>
-      </Route>
-      <Route exact path="/">
-        <>
-          <button
-            className={`header__burger-button ${
-              burgerIsOpened && "header__burger-button_opened"
-            }`}
-            type="button"
-            aria-label="показать/скрыть меню"
-            onClick={handleBurgerClick}
-          />
-          <div
-            className={`header__menu ${
-              burgerIsOpened && "header__menu_opened"
-            }`}>
-            <p className="header__email">{email}</p>
+      <Switch>
+        <Route path="/sign-in">
+          <Link to="/sign-up" className="header__link">
+            Регистрация
+          </Link>
+        </Route>
+        <Route path="/sign-up">
+          <Link to="/sign-in" className="header__link">
+            Войти
+          </Link>
+        </Route>
+        <Route exact path="/">
+          <>
             <button
-              className="header__logout-button"
+              className={`header__burger-button ${
+                burgerIsOpened && "header__burger-button_opened"
+              }`}
               type="button"
-              aria-label="Выйти"
-              onClick={() => onLogout()}>
-              Выйти
-            </button>
-          </div>
-        </>
-      </Route>
+              aria-label="показать/скрыть меню"
+              onClick={handleBurgerClick}
+            />
+            <div
+              className={`header__menu ${
+                burgerIsOpened && "header__menu_opened"
+              }`}>
+              <p className="header__email">{email}</p>
+              <button
+                className="header__logout-button"
+                type="button"
+                aria-label="Выйти"
+                onClick={() => onLogout()}>
+                Выйти
+              </button>
+            </div>
+          </>
+        </Route>
+      </Switch>
     </header>
   );
 }
